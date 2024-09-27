888832ffb6fe953ef8fe2f0a41499e293349fa47
vetdy03 <cocaever0@gmail.com>
2024-09-27 16:55:24 -0400

    celular aumnetado

---
+++ Celular.py
@@ -0,0 +1,10 @@
+class Celular:
+    def __init__(self, marca, modelo, camara):
+        self.marcaa = marca
+        self.modeloo = modelo
+        self.camaraa = marca
+celular1 = Celular("samsung", "s23", "48mp")
+
+#celular1.marcaa = "aplle"
+print(celular1.marcaa)
+
