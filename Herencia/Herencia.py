09bd2f65523686923c60248290f89b8e591cb4a1
vetdy03 <cocaever0@gmail.com>
2024-10-03 19:51:23 -0400

    agregando arraigoEsVocal

---
+++ Herencia/Herencia.py
@@ -0,0 +1,13 @@
+class Persona:
+    def __init__(self, nombre, edad, nacionalidad):
+        self.nombre = nombre
+        self.edad = edad
+        self.nacionalidad = nacionalidad
+        
+class Empleado(Persona):
+    pass
+
+def saludar():
+    pass
+
+saludar()        
