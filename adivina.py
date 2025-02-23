53596711e5467b731cbf98896a353d11c5346808
vetdy03 <cocaever0@gmail.com>
2025-02-22 23:25:53 -0400

    subiendo juego de adivinar

--- adivina.py
+++ adivina.py
@@ -4,22 +4,22 @@ def main():
     secret = random.randint(1, 100)
     print('A D I V I N A   E L   N U M E R O   S E C R E T O ')
     number = int(input('Ingrese su numero: '))
-    attempt = 10
+    attempt = 5
 
 
 
     while number != secret and attempt > 0:
         if number > secret:
-            print('Por encima del numero secreto todavia tienes {} intentos'.format(attempt))
+            print('Tu numero secreto es menor, todavia tienes {} intentos'.format(attempt))
             number = int(input('Intenta otra vez: '))
             attempt -= 1
         elif number < secret:
-            print('Por debajo del numero secreto todavia tienes {} intentos'.format(attempt)) 
+            print('Tu numero secreto es mayor, todavia tienes {} intentos'.format(attempt)) 
             number = int(input('Intenta otra vez: '))
             attempt -= 1
     
     if number == secret:
-        print('Felicidades adivinaste el numero secreto')
+        print('Felicidades abet adivinaste el numero secreto')
     else:
         print('Perdiste eres manco como yahir 17, el numero fue: ')
     print(secret)
