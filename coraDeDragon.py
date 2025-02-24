498b6b550431dc7a7ff6932da6d57b28f105d4c5
vetdy03 <cocaever0@gmail.com>
2025-02-23 23:54:46 -0400

    corazon de dragon

---
+++ coraDeDragon.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+def main():
+    letra = int(input('Escriba la inicial del animal '))
+    number1 = int(input('Escriba el numero'))
+    #nombreDaria 
+    #   
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
