b533afd411bedb425bfe40d8a13a7522e3bc9d3c
vetdy03 <cocaever0@gmail.com>
2025-02-28 21:18:06 -0400

    clase 1 subeindo PRACTICA

---
+++ clase1.py
@@ -0,0 +1,13 @@
+def main():
+    numero = int(input("Ingrese su numero"))
+    x, y= 4, 6
+    resultado= int(x+y)
+    print(resultado)
+    
+    l, m = 4, 7
+    resultado2 = l+m
+    print(resultado+resultado2)
+    
+
+if __name__ == '__main__':
+    main()
