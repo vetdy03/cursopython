dceed50e5513c421a7ecf028398e855bc40c0716
vetdy03 <cocaever0@gmail.com>
2025-02-28 21:52:28 -0400

    nueva clase ejercico 1 de clasesextra

---
+++ ejer1cd.py
@@ -0,0 +1,10 @@
+def main():
+    name = input("ingrese su name: ")
+    num = int(input("Ingrese su numero: "))
+    print((name + "\n")*num)
+    
+   
+    
+
+if __name__ == '__main__':
+    main()
