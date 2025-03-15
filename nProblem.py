0bb811f7c636f97a4aa074f1b56cb22c9450629c
vetdy03 <cocaever0@gmail.com>
2025-03-14 23:17:44 -0400

    agregando imc yt nproblem

---
+++ nProblem.py
@@ -0,0 +1,8 @@
+def nProblemSuma():
+    n= int(input("Ingrese su numero"))
+    suma= (n*(n+1))/2
+    #probando con 2 res: 3
+    print("La respuesta de la suma {} es ".format(suma))
+    #print('La factorial de {} es {} '.format(numberCopy, sumador))
+if __name__ == '__main__':
+    nProblemSuma()
