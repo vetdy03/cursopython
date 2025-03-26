c4a379e961e7ec9301a3d55fc1323d676cc25328
vetdy03 <cocaever0@gmail.com>
2025-03-26 00:11:29 -0400

    refactorizando el ejercicio4.py

--- stringss/ejer4.py
+++ stringss/ejer4.py
@@ -1,6 +1,10 @@
 
 def phoneNumbers():
     number = input("Ingrese su numero con xodigo de pais\n número y prefijo: ")
+    number2 = input("Ingrese su segundo numero de telefono: ")
     print("Su numero de telefono es: {} ".format(number[4:13]))
+    print("Su segundo numero purito es: {}".format(number2[4:-3])) 
+    #+54-769589387-56
+    #0123456789012345
 if __name__ ==  '__main__':
     phoneNumbers()
