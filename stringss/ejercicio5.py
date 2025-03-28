43b0477732bc6d4c6943ce935b2e60712f190e50
vetdy03 <cocaever0@gmail.com>
2025-03-28 00:22:59 -0400

    mejorando el codigo refac

--- stringss/ejercicio5.py
+++ stringss/ejercicio5.py
@@ -2,5 +2,6 @@ def invertirFrase():
     phrase = input("Enter your phrase: ")
     print("Your inverted phrase is: {}".format(phrase[::-1]))
     print("See you back soon!!!")
+    
 if __name__ == '__main__':
     invertirFrase()
