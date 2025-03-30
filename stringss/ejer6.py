41b9dedbb1c54d0e6c2eeaef7a2d593c1ba816bc
vetdy03 <cocaever0@gmail.com>
2025-03-29 23:36:17 -0400

    mejorando el ejercico 6

--- stringss/ejer6.py
+++ stringss/ejer6.py
@@ -1,4 +1,7 @@
 def replacetovocal():
-    pass
+    phrase= input("Enter your phrase: ")
+    vocal = input("Enter your vocal: ")
+    print("Your phrase inverted is: {}".format(phrase.replace(vocal, vocal.upper())))
+    
 if __name__ == '__main__':
     replacetovocal()
