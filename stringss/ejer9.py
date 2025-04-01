03e6f069d650ed8eeebe1fe0d0e6e57164717b5d
vetdy03 <cocaever0@gmail.com>
2025-04-01 00:07:31 -0400

    subieno el ejercico 9 de la tareas. VETDY DEBES DE MEJORAR SI O SI, REFACTOR

--- stringss/ejer9.py
+++ stringss/ejer9.py
@@ -1,4 +1,9 @@
-fecha = input("Introduce la fecha de tu nacimiento en formato dd/mm/aaaa: ")
-print('Día', fecha[:2])
-print('Mes', fecha[3:5])
-print('Año', fecha[6:])
+#Escribir un programa que pregunte al usuario la fecha de su nacimiento en formato dd/mm/aaaa y muestra por pantalla, el día, el mes y el año.
+def birthdate():
+    age = input("Enter your date of birth: ")
+    print('Día', age[:3])
+    print('Mes', age[3:5])
+    print('Año', age[5:])
+    #23/10/1996
+if __name__ == '__main__':
+    birthdate()
