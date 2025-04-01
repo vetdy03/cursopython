181bbc054b92eed96ffa1049e982da244f4fce8d
vetdy03 <cocaever0@gmail.com>
2025-03-31 23:57:57 -0400

    agrenago el ejercicio 9 de la tarea y refactorizado

--- stringss/ejer9.py
+++ stringss/ejer9.py
@@ -1,8 +1,4 @@
-fecha = input("Introduce la fecha de tu nacimiento en formato día/mes/año: ")
-dia = fecha[:fecha.find('/')]
-mesaño = fecha[fecha.find('/')+1:]
-mes = mesaño[:mesaño.find('/')]
-año = mesaño[mesaño.find('/')+1:]
-print('Día', dia)
-print('Mes', mes)
-print('Año', año)
+fecha = input("Introduce la fecha de tu nacimiento en formato dd/mm/aaaa: ")
+print('Día', fecha[:2])
+print('Mes', fecha[3:5])
+print('Año', fecha[6:])
