0c7bf1619aa123b83fc7727ebfc6985e63dbe441
vetdy03 <cocaever0@gmail.com>
2025-04-06 23:59:11 -0400

    subiendo el ejercicio 12 v1

--- stringss/DELejer12.py
+++ stringss/DELejer12.py
@@ -1,8 +1,8 @@
-Escribir un programa que pregunte el nombre el un producto, su precio y un número de unidades y muestre por pantalla una cadena con el siguiente formato:
+#Escribir un programa que pregunte el nombre el un producto, su precio y un número de unidades y muestre por pantalla una cadena con el siguiente formato:
 
-<producto>: <unidades> unidades x <precio>€ = <total>€
+#<producto>: <unidades> unidades x <precio>€ = <total>€
 
-donde <unidades> es el número de unidades con cinco dígitos, <precio> es el precio unitario con 6 dígitos enteros y 2 decimales y <total> es el coste total con 8 dígitos enteros y 2 decimales.
+#donde <unidades> es el número de unidades con cinco dígitos, <precio> es el precio unitario con 6 dígitos enteros y 2 decimales y <total> es el coste total con 8 dígitos enteros y 2 decimales.
 producto = input('Introduce el nombre del producto: ')
 precio = float(input('Introducde el precio unitario: '))
 unidades = int(input('Introduce el número de unidades: '))
