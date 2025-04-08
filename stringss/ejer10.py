35cdcc9336983b407d942deca1991b0bb2d7aab2
vetdy03 <cocaever0@gmail.com>
2025-04-07 22:54:41 -0400

    corrigiendo el erjcicio 10

--- stringss/ejer10.py
+++ stringss/ejer10.py
@@ -1,7 +1,9 @@
 #Escribir un programa que pregunte por consola por los productos de una cesta de la compra, separados por comas, y muestre por pantalla cada uno de los productos en una línea distinta.
 def getProducts():
+    #cesta = input('Introduce los productos de la cesta de la compra separados por comas: ')
+    #print('\n'.join(cesta.split(',')))
     cesta = input('Introduce los productos de la cesta de la compra separados por comas: ')
-print('\n'.join(cesta.split(',')))
+    print(cesta.replace(',', '\n'))
     
 if __name__ == '__main':
     getProducts()
