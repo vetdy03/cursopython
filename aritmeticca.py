eb3e8f26714bd57050dfbaef6bbb0fab893eb391
ever coca <86172270+vetdy03@users.noreply.github.com>
2025-04-09 23:55:53 -0400

    ejercicios de suma y multi

---
+++ aritmeticca.py
@@ -0,0 +1,8 @@
+#Descripción: Realiza operaciones aritméticas básicas como suma y multiplicación.
+#Objetivo: Entender cómo realizar operaciones matemáticas y usar variables.
+numero1 = 5
+numero2 = 3
+suma = numero1 + numero2
+multiplicacion = numero1 * numero2
+print("Suma:", suma)
+print("Multiplicación:", multiplicacion)
