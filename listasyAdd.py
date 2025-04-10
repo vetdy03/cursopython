20286aa83788771687f6b8eeb125d9e5993e89e3
ever coca <86172270+vetdy03@users.noreply.github.com>
2025-04-09 23:57:10 -0400

    Create listasyAdd.py vetdy revisalo

---
+++ listasyAdd.py
@@ -0,0 +1,10 @@
+# Creando Listas y Agregando Elementos
+
+#Descripción: Aprende a crear una lista y agregar elementos a ella.
+#Objetivo: Comprender las listas, una de las estructuras de datos más utilizadas en Python.
+
+mi_lista = []
+mi_lista.append(1)
+mi_lista.append(2)
+mi_lista.append(3)
+print(mi_lista)
