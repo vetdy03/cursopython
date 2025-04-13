1750f9b21fc843ac17e813984d1b0a5b1114b0d7
ever coca <86172270+vetdy03@users.noreply.github.com>
2025-04-12 23:25:15 -0400

    Create petsfind.py buscr mascotas

---
+++ petsfind.py
@@ -0,0 +1,6 @@
+#Descripción: Utiliza un bucle for para iterar sobre los elementos de una lista.
+#Objetivo: Aprender a usar bucles, fundamentales para automatizar y repetir tareas.
+#Cada uno de estos ejercicios te introducirá a conceptos clave de la programación en Python de una manera práctica y sencilla. No te preocupes si no entiendes todo de inmediato. La práctica constante es la clave para dominar cualquier habilidad.
+animales = ["gato", "perro", "pájaro"]
+for animal in animales:
+    print("Tengo un", animal)
