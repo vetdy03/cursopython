e07f4bdfb5883a7a1bae0729ee5387969e9bad98
ever coca <86172270+vetdy03@users.noreply.github.com>
2025-04-13 22:24:24 -0400

    Update ejercicio11.py mejorando porque si y si me dio la gana

--- stringss/ejercicio11.py
+++ stringss/ejercicio11.py
@@ -1,1 +1,5 @@
 #escribir un programa que invierta un numero
+producto = input('Introduce el nombre del producto: ')
+precio = float(input('Introducde el precio unitario: '))
+unidades = int(input('Introduce el número de unidades: '))
+print('{producto}: {unidades:3d} unidades x {precio:9.2f}€ = {total:11.2f}€'.format(producto = producto, unidades = unidades, precio = precio, total = unidades * precio))
