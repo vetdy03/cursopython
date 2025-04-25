6a5b360353bde7bb46e8868bf1073fd1f8e073d4
ever coca <86172270+vetdy03@users.noreply.github.com>
2025-04-24 23:58:50 -0400

    Create forenpython.py fors

---
+++ forenpython.py
@@ -0,0 +1,8 @@
+Bucles For: Iterando sobre una Lista
+
+Descripción: Utiliza un bucle for para iterar sobre los elementos de una lista.
+Objetivo: Aprender a usar bucles, fundamentales para automatizar y repetir tareas.
+Código de Ejemplo:
+animales = ["gato", "perro", "pájaro"]
+for animal in animales:
+    print("Tengo un", animal)
