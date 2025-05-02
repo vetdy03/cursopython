a1d057e2c7e903211912b50b17177e85ce57cfcd
ever coca <86172270+vetdy03@users.noreply.github.com>
2025-05-01 23:53:33 -0400

    ciclos numeros del 1 al 20

---
+++ ejer5.py
@@ -0,0 +1,15 @@
+#Ejercicio 5: Crear una lista de números pares del 1 al 20 usando un bucle
+#Objetivo: Enseñar cómo usar bucles (for) y lógica condicional.
+In [6]:
+# Crear una lista vacía para los números pares
+lista_pares = []
+# Usar un bucle para iterar de 1 a 20
+El cuadrado de 15 es 225
+Diccionario: {'Lenguaje': 'Python', 'Biblioteca': 'Pandas', 'Visualización': 'Seaborn'}
+Introduce un número: 10
+El número es positivo
+for num in range(1, 21):
+if num % 2 == 0: # Verificar si el número es par
+lista_pares.append(num)
+# Mostrar la lista de números pares
+print(lista_pares)
