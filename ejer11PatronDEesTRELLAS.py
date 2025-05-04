4ebb5360868bcd93aa220b636377237296830e9e
ever coca <86172270+vetdy03@users.noreply.github.com>
2025-05-04 19:04:07 -0400

    patron de estrellas

---
+++ ejer11PatronDEesTRELLAS.py
@@ -0,0 +1,10 @@
+#Ejercicio 11: Usar un bucle para imprimir un patrón de estrellas
+#Objetivo: Practicar bucles anidados y la creación de patrones.
+In [19]:
+# --- Patrón creciente ---
+# Definir el número de líneas del patrón
+n = 5
+# Usar un bucle para imprimir el patrón de estrellas
+for i in range(1, n + 1):
+for j in range(i):
+print(
